52f1b38d1b3e07f95707cae4e3785fe89894f3e0
Ayaka <73595362+USERSATOSHI@users.noreply.github.com>
2021-10-12 18:14:23 +0530

    IndexMap half done , Binary idk what to do

--- dist/binary/binary.ts
+++ dist/binary/binary.ts
@@ -1,5 +1,7 @@
 type ByteData = Array<any> | string | BinaryData
-
+type BufferData = {
+    offset: number
+}
 class Byte {
     name: string;
     size: number;
@@ -9,7 +11,7 @@ class Byte {
         this.name = name;
         this.size = size;
         this.type = type;
-        this.data = type === 'text' ? [] : type === "number" ? (00) : type === "buffer" ? new DataView(new ArrayBuffer(size)) : undefined;
+        this.data = type === 'text' ? [] : type === "number" ? (00) : type === "buffer" ? : undefined;
     }
 }
 export default class Binary {
@@ -34,13 +36,26 @@ export default class Binary {
     }
     /**
      * @method addByteData
+     * @description adds data to that byte
+     * @param name name of the byte
+     * @param data 
+     * @return data
      */
-    public addByteData(name: string, data: any) {
+    public addByteData(name: string, data: any, bufferMethod: BufferData) {
         const byte = this.data[name];
         if (!byte) throw new Error("Byte With Name:" + name + " Doesn't Exist!");
 
         const type = byte.type;
-        if (typeof data !== type) throw new Error("Data Provided Is not Od Same Type");
+        if (typeof data !== type) throw new Error("Data Provided Is not Of Same Type");
 
+        if (type === 'string') {
+            byte.data = data.split('').map(x => x.charCodeAt(0).toString(2));
+        }
+        else if (type === 'data') {
+            byte.data = Number(data).toString(2);
+        }
+        else {
+            byte.data.setInt32(bufferMethod.offset, data);
+        }
     }
 }
---
+++ dist/indexed/indexMap.ts
@@ -0,0 +1,131 @@
+type Character = 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h' | 'i' | 'j' | 'k' | 'l' | 'm' | 'n' | 'o' | 'p' | 'q' | 'r' | 's' | 't' | 'u' | 'v' | 'w' | 'x' | 'y' | 'z';
+
+class IndexData<K = string, V = Array<number>> {
+    letters: Record<Character, Array<number>>;
+    length: number;
+
+    constructor(word: string) {
+        const wordArray = word.split('');
+
+        Object.defineProperty(this, 'letters', { value: {} });
+        Object.defineProperty(this, 'length', { value: wordArray.length });
+
+        wordArray.forEach((x, y) => {
+            const LetterIndexes = this.letter[x];
+
+            if (LetterIndexes) {
+                LetterIndexes.push(y);
+            }
+            else {
+                LetterIndexes = [y];
+            }
+        });
+    }
+    /**
+     * @method has
+     * @description returnds whether a letter exist in the IndexData
+     * @param letter letter to be checked
+     * @Complexity best : O(1) | worst : O(1)
+     * @return boolean
+     */
+    public has(letter: Character): boolean {
+        return this.letters[letter] ? true : false;
+    }
+    /**
+     * @method add
+     * @description adds the letter to the Data
+     * @param letter letter to be added
+     * @Complexity best : O(1) | worst : O(1)
+     * @return boolean
+     */
+    public add(letter: Character): number[] {
+        const LetterPlaces = this.letters[letter];
+        if (!LetterPlaces) LetterPlaces = [this.length];
+        else LetterPlaces.push(this.length);
+        this.length += 1
+        return LetterPlaces;
+    }
+    /**
+     * @method remove
+     * @description removes the last index of that letter if it exists
+     * @param letter letter to be removed
+     * @Complexity best : O(1) | worst : O(n)
+     * @return new number[]
+     */
+    public remove(letter: Character): number[] {
+        if (!this.has(letter)) return [-1];
+        else {
+            const indexes = this.letters[letter];
+            indexes.pop();
+
+            this.length -= 1;
+
+            return indexes;
+        }
+    }
+    /**
+     * @method entries
+     * @description return data as Entries
+     * @Complexity best : O(n) | worst : O(n)
+     * @return [string,number[]][]
+     */
+    public entries(): [string, number[]][] {
+        return Object.entries(this.letters);
+    }
+    /**
+     * @method toString
+     * @description Converts the Data into String and returns it
+     * @Complexity best : O(n) | worst : O(n^2)
+     * @return string
+     */
+    public toString(): string {
+        const string: string[] = [];
+
+        for (const [key, value] of this.entries()) {
+            if (value.length === 1) {
+                string[value[0]] = key;
+            }
+            else {
+                let i = value.length - 1;
+                while (i >= 0) {
+                    string[value[i]] = key;
+                    i--
+                }
+            }
+        }
+        return string.join('');
+    }
+    /**
+     * @method map
+     * @description maps a function over the data and return the resultant array
+     * @Complexity best : O(n^2) | worst : O(n^2)
+     * @return U[]
+     */
+    public map<U>( func : ( indexes : V,letter : K, Data : IndexData ) => U ) : U[] {
+        const res = [];
+        for( const [ key,value ] of this.entries() ) {
+            res.push( func(value,key,this) )
+        }
+        return res;
+    }
+    /**
+     * @method forEach
+     * @description loops a function over the data
+     * @Complexity best : O(n^2) | worst : O(n^2)
+     * @return void
+     */
+     public forEach( func : ( indexes : V,letter : K, Data : IndexData ) => void ) : void {
+        for( const [ key,value ] of this.entries() ) {
+        func(value,key,this);
+        }
+    }
+    /**
+     * @method size
+     * @description returns the size of keys
+     * @Complexity best : <=O(n) | worst : O(n)
+     * @return number
+     */
+    public get size() : number {
+        return Object.keys( this.letters ).length;
+    }
+}
