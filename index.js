c925006938b2094068e77811617e2f3706cf4ff8
USERSATOSHI <user.satoshi69@gmail.com>
2021-10-13 20:18:48 +0530

    yes

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
-const Binary = require('./src/binary/binary').default;
-
-const b = new Binary(10);
-b.newByte('testByte', "number", 64);
-b.addByteData('testByte', 1234)
-
-console.log({ b, getRawData: b.getRawData("testByte"), getData: b.getData('testByte') })
+const typedoc = require('typedoc');
+require('child_process').exec( 'typedoc ./dist' )
