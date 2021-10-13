67991ea359f6d9717ce357994ec9161c6cdd02a8
USERSATOSHI <user.satoshi69@gmail.com>
2021-10-13 20:55:47 +0530

    y

---
+++ dist/index.ts
@@ -0,0 +1,6 @@
+import Group from "./group/group";
+import IndexData from "./indexed/indexMap";
+import Binary from "./binary/binary";
+import Tree from "./tree/tree";
+
+export default { Group, IndexData, Binary, Tree }
