e9a62ac63433f194d68bfcbd16aba24a75372d00
Ayaka <user.satoshi69@gmail.com>
2021-10-20 15:06:51 +0530

    changes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export * as Binary from './binary/binary'
 export * as Group from './group/group'
-export * as IndexData from 'dist/indexed/indexMap'
+export * as IndexData from './indexed/indexMap'
 export * as Tree from './tree/tree'
