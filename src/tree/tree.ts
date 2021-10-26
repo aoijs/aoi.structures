c29e203937a56d6276662e5ba2f63b4d212c71e4
Ayaka <user.satoshi69@gmail.com>
2021-10-26 22:40:45 +0530

    yes

--- src/tree/tree.ts
+++ src/tree/tree.ts
@@ -23,4 +23,43 @@ export class Tree {
         this.branches.set(name, newBranch);
         return newBranch;
     }
+    /**
+     * @method deleteBranch
+     * @description delete a Branch in the Tree
+     * @param name name of the Branch
+     * @return void
+     */
+    public deleteBranch(name: string): void {
+        if (!this.branches.has(name))
+            throw new Error("Branch With Name: " + name + " Doesn't Exist");
+        else this.branches.delete(name);
+    }
+    /**
+     * @method clear
+     * @description clears the Tree
+     * @return void
+     */
+    public clear(): void {
+        this.branches.clear();
+    }
+    /**
+     * @method clearBranch
+     * @description clears a Branch in the Tree
+     * @param name name of the Branch
+     * @return void
+     */
+    public clearBranch(name: string): void {
+        if (!this.branches.has(name))
+            throw new Error("Branch With Name: " + name + " Doesn't Exist");
+        else this.branches.set(name, new Branch(name, this));
+    }
+    /**
+     * @method branchCount
+     * @description returns the number of branches in the tree
+     * @readonly
+     * @return number
+     */
+    public get branchCount(): number {
+        return this.branches.size;
+    }
 }
