31198f3cf24c07ddaa77af188df146ff23f94b44
Ayaka <user.satoshi69@gmail.com>
2021-12-05 23:10:53 +0530

    updated

---
+++ dist/src/linked/node.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Node {
+    constructor(element) {
+        this.element = element;
+        this.next = null;
+    }
+}
+exports.default = Node;
+//# sourceMappingURL=node.js.map
